6219d27d03918e7775a1add896204d1dbd95c0e6
Benjamin <benji.johansen@gmail.com>
2023-11-16 18:38:42 +0100

    added error check

--- scripts/endocrin_scraper.py
+++ scripts/endocrin_scraper.py
@@ -9,15 +9,17 @@ response = requests.get(url)
 # Parse the HTML content
 soup = BeautifulSoup(response.content, "html.parser")
 
-#TODO: Create a function to loop through the headers. 
+# TODO: Create a function to loop through the headers.
 
-def scrape_endo_guidelines(output_file_path:str):
+
+def scrape_endo_guidelines(output_file_path: str):
     """
     Loop through the file
     """
     # Local header variables
-    header_vars = ["h1","h2", "h3"]
-
+    header_vars = ["h1", "h2", "h3"]
+    if not isinstance(output_file_path, str):
+        raise TypeError("output_file_path must be a string")
     with open(output_file_path, "w") as f:
         for header in soup.find_all(header_vars):
             for elem in header.next_siblings:
@@ -27,11 +29,16 @@ def scrape_endo_guidelines(output_file_path:str):
                     f.write(f"## {elem.get_text()}\n")
                 elif elem.name == header_vars[2]:
                     f.write(f"### {elem.get_text()}\n")
-                elif elem.name == "p" and elem.next_sibling and elem.next_sibling.name == "p":
+                elif (
+                    elem.name == "p"
+                    and elem.next_sibling
+                    and elem.next_sibling.name == "p"
+                ):
                     f.write(elem.get_text() + "\n")
                 else:
                     f.write(elem.get_text() + "\n\n")
         print(f"Done scraping {url}.\n Saved to: {output_file}")
+
 
 # %%
 # Save the file to the designated filename
